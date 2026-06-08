21ed98aa1650fc6d350ce85418f74e6c473881ea
Your Name <izadi.afsaneh1390@gmail.com>
2026-06-08 15:18:23 +0330

    تغییرات اصلی در اپتیمایز اکتیویتی و تغییرات لایه

--- app/src/main/java/com/example/abgineh/MainActivity.java
+++ app/src/main/java/com/example/abgineh/MainActivity.java
@@ -247,40 +247,40 @@ public class MainActivity extends AppCompatActivity {
 //                    Toast.makeText(MainActivity.this, "بهینه انجام شد", Toast.LENGTH_SHORT).show();
                });
 
-        btnNext.setOnClickListener(
-                view -> {
+//        btnNext.setOnClickListener(
+//                view -> {
+//
+//                    if (sheets == null) return;
+//
+//                    if (currentSheet
+//                            < sheets.size() - 1) {
+//
+//                        currentSheet++;
+//
+//                        showSheet(currentSheet);
+//                    }
+//                });
+//
+//        btnPrev.setOnClickListener(
+//                view -> {
+//
+//                    if (sheets == null) return;
+//
+//                    if (currentSheet > 0) {
+//
+//                        currentSheet--;
+//
+//                        showSheet(currentSheet);
+//                    }
+//                });
 
-                    if (sheets == null) return;
-
-                    if (currentSheet
-                            < sheets.size() - 1) {
-
-                        currentSheet++;
-
-                        showSheet(currentSheet);
-                    }
-                });
-
-        btnPrev.setOnClickListener(
-                view -> {
-
-                    if (sheets == null) return;
-
-                    if (currentSheet > 0) {
-
-                        currentSheet--;
-
-                        showSheet(currentSheet);
-                    }
-                });
-
-        btnPdf.setOnClickListener(
-                view -> {
-
-                    if (sheets != null) {
-                        exportPdf();
-                    }
-                });
+//        btnPdf.setOnClickListener(
+//                view -> {
+//
+//                    if (sheets != null) {
+//                        exportPdf();
+//                    }
+//                });
     }
 
 @SuppressLint("NotifyDataSetChanged")
@@ -341,236 +341,236 @@ private void LoadProject(int id){
 }
 
 
-    @SuppressLint({"SetTextI18n", "DefaultLocale"})
-    private void showSheet(int index) {
-
-        List<Placement> placements =
-                sheets.get(index);
-
-        sheetView.sheetWidth = 3210;
-        sheetView.sheetHeight = 2250;
-
-        sheetView.placements = placements;
-        sheetView.setPageInfo(index, sheets.size());
-
-        sheetView.invalidate();
-
-        double waste =
-                optimizer.calculateWaste(
-                        placements
-                );
-        pageNum.setText((index + 1) + " از " + sheets.size());
-
-    }
+//    @SuppressLint({"SetTextI18n", "DefaultLocale"})
+//    private void showSheet(int index) {
+//
+//        List<Placement> placements =
+//                sheets.get(index);
+//
+//        sheetView.sheetWidth = 3210;
+//        sheetView.sheetHeight = 2250;
+//
+//        sheetView.placements = placements;
+//        sheetView.setPageInfo(index, sheets.size());
+//
+//        sheetView.invalidate();
+//
+//        double waste =
+//                optimizer.calculateWaste(
+//                        placements
+//                );
+//        pageNum.setText((index + 1) + " از " + sheets.size());
+//
+//    }
 
     @SuppressLint("SetTextI18n")
 
-    private void exportPdf() {
-
-        PdfDocument document =
-                new PdfDocument();
-
-        int pageNum = 1;
-
-        Random random =
-                new Random();
-
-        for (List<Placement> sheet : sheets) {
-
-            PdfDocument.PageInfo pageInfo =
-                    new PdfDocument.PageInfo.Builder(
-                            1200,
-                            1600,
-                            pageNum
-                    ).create();
-
-            PdfDocument.Page page =
-
-                    document.startPage(pageInfo);
-
-            Canvas canvas =
-                    page.getCanvas();
-
-            Paint fillPaint =
-                    new Paint();
-
-            Paint borderPaint =
-                    new Paint();
-
-            Paint textPaint =
-                    new Paint();
-
-            borderPaint.setStyle(
-                    Paint.Style.STROKE
-            );
-
-            borderPaint.setStrokeWidth(4);
-
-            textPaint.setColor(Color.BLACK);
-
-
-            textPaint.setTextSize(28);
-
-            int margin = 80;
-
-            float scaleX =
-                    (1200f - margin * 2)
-                            / optimizer.sheetWidth;
-
-            float scaleY =
-                    (1500f - margin * 2)
-                            / optimizer.sheetHeight;
-
-            float scale =
-                    Math.min(scaleX, scaleY);
-
-            // کادر شیت
-
-            canvas.drawRect(
-                    margin,
-                    margin,
-
-
-                    margin +
-                            optimizer.sheetWidth * scale,
-
-                    margin +
-                            optimizer.sheetHeight * scale,
-
-                    borderPaint
-            );
-
-            for (Placement p : sheet) {
-
-                fillPaint.setColor(
-                        Color.rgb(
-                                random.nextInt(200),
-                                random.nextInt(200),
-                                random.nextInt(200)
-                        )
-                );
-
-                float left =
-
-                        margin + p.x * scale;
-
-                float top =
-                        margin + p.y * scale;
-
-                float right =
-                        margin +
-                                (p.x + p.width) * scale;
-
-                float bottom =
-                        margin +
-                                (p.y + p.height) * scale;
-
-                // رنگ
-
-                canvas.drawRect(
-                        left,
-                        top,
-                        right,
-                        bottom,
-                        fillPaint
-
-                );
-
-                // خط دور
-
-                if (p.rotated) {
-
-                    borderPaint.setColor(
-                            Color.RED
-                    );
-
-                } else {
-
-                    borderPaint.setColor(
-                            Color.BLACK
-                    );
-                }
-
-                canvas.drawRect(
-                        left,
-                        top,
-                        right,
-
-                        bottom,
-                        borderPaint
-                );
-
-                // متن ابعاد
-
-                canvas.drawText(
-
-                        p.width +
-                                "×" +
-                                p.height,
-
-                        left + 10,
-
-                        top + 35,
-
-                        textPaint
-                );
-            }
-
-            // عنوان صفحه
-
-
-            canvas.drawText(
-
-                    "Sheet " + pageNum,
-
-                    80,
-
-                    40,
-
-                    textPaint
-            );
-
-            document.finishPage(page);
-
-            pageNum++;
-        }
-
-        try {
-
-            File file =
-                    new File(
-
-
-                            getExternalFilesDir(
-                                    Environment
-                                            .DIRECTORY_DOCUMENTS
-                            ),
-
-                            "glass_output.pdf"
-                    );
-
-            document.writeTo(
-                    new FileOutputStream(file)
-            );
-
-            document.close();
-
-            sharePdf(file);
-
-//            txtInfo.setText(
-//                    "PDF ساخته شد"
+//    private void exportPdf() {
+//
+//        PdfDocument document =
+//                new PdfDocument();
+//
+//        int pageNum = 1;
+//
+//        Random random =
+//                new Random();
+//
+//        for (List<Placement> sheet : sheets) {
+//
+//            PdfDocument.PageInfo pageInfo =
+//                    new PdfDocument.PageInfo.Builder(
+//                            1200,
+//                            1600,
+//                            pageNum
+//                    ).create();
+//
+//            PdfDocument.Page page =
+//
+//                    document.startPage(pageInfo);
+//
+//            Canvas canvas =
+//                    page.getCanvas();
+//
+//            Paint fillPaint =
+//                    new Paint();
+//
+//            Paint borderPaint =
+//                    new Paint();
+//
+//            Paint textPaint =
+//                    new Paint();
+//
+//            borderPaint.setStyle(
+//                    Paint.Style.STROKE
 //            );
-
-
-        } catch (Exception e) {
-
-            e.printStackTrace();
-
-//            txtInfo.setText(
-//                    e.getMessage()
+//
+//            borderPaint.setStrokeWidth(4);
+//
+//            textPaint.setColor(Color.BLACK);
+//
+//
+//            textPaint.setTextSize(28);
+//
+//            int margin = 80;
+//
+//            float scaleX =
+//                    (1200f - margin * 2)
+//                            / optimizer.sheetWidth;
+//
+//            float scaleY =
+//                    (1500f - margin * 2)
+//                            / optimizer.sheetHeight;
+//
+//            float scale =
+//                    Math.min(scaleX, scaleY);
+//
+//            // کادر شیت
+//
+//            canvas.drawRect(
+//                    margin,
+//                    margin,
+//
+//
+//                    margin +
+//                            optimizer.sheetWidth * scale,
+//
+//                    margin +
+//                            optimizer.sheetHeight * scale,
+//
+//                    borderPaint
 //            );
-        }
-    }
+//
+//            for (Placement p : sheet) {
+//
+//                fillPaint.setColor(
+//                        Color.rgb(
+//                                random.nextInt(200),
+//                                random.nextInt(200),
+//                                random.nextInt(200)
+//                        )
+//                );
+//
+//                float left =
+//
+//                        margin + p.x * scale;
+//
+//                float top =
+//                        margin + p.y * scale;
+//
+//                float right =
+//                        margin +
+//                                (p.x + p.width) * scale;
+//
+//                float bottom =
+//                        margin +
+//                                (p.y + p.height) * scale;
+//
+//                // رنگ
+//
+//                canvas.drawRect(
+//                        left,
+//                        top,
+//                        right,
+//                        bottom,
+//                        fillPaint
+//
+//                );
+//
+//                // خط دور
+//
+//                if (p.rotated) {
+//
+//                    borderPaint.setColor(
+//                            Color.RED
+//                    );
+//
+//                } else {
+//
+//                    borderPaint.setColor(
+//                            Color.BLACK
+//                    );
+//                }
+//
+//                canvas.drawRect(
+//                        left,
+//                        top,
+//                        right,
+//
+//                        bottom,
+//                        borderPaint
+//                );
+//
+//                // متن ابعاد
+//
+//                canvas.drawText(
+//
+//                        p.width +
+//                                "×" +
+//                                p.height,
+//
+//                        left + 10,
+//
+//                        top + 35,
+//
+//                        textPaint
+//                );
+//            }
+//
+//            // عنوان صفحه
+//
+//
+//            canvas.drawText(
+//
+//                    "Sheet " + pageNum,
+//
+//                    80,
+//
+//                    40,
+//
+//                    textPaint
+//            );
+//
+//            document.finishPage(page);
+//
+//            pageNum++;
+//        }
+//
+//        try {
+//
+//            File file =
+//                    new File(
+//
+//
+//                            getExternalFilesDir(
+//                                    Environment
+//                                            .DIRECTORY_DOCUMENTS
+//                            ),
+//
+//                            "glass_output.pdf"
+//                    );
+//
+//            document.writeTo(
+//                    new FileOutputStream(file)
+//            );
+//
+//            document.close();
+//
+//            sharePdf(file);
+//
+////            txtInfo.setText(
+////                    "PDF ساخته شد"
+////            );
+//
+//
+//        } catch (Exception e) {
+//
+//            e.printStackTrace();
+//
+////            txtInfo.setText(
+////                    e.getMessage()
+////            );
+//        }
+//    }
 
     private void saveProject() {
         String name = nameproject.getText().toString().trim();
@@ -610,33 +610,33 @@ private void LoadProject(int id){
 
 //            txtInfo.setText("پروژه ذخیره شد");
         }
-    private void sharePdf(File file) {
-
-        Uri uri =
-                FileProvider.getUriForFile(
-                        this,
-                        getPackageName() + ".provider",
-                        file
-                );
-        Intent intent =
-                new Intent(Intent.ACTION_SEND);
-
-        intent.setType("application/pdf");
-
-        intent.putExtra(
-                Intent.EXTRA_STREAM,
-                uri
-        );
-
-        intent.addFlags(
-                Intent.FLAG_GRANT_READ_URI_PERMISSION
-        );
-
-        startActivity(
-                Intent.createChooser(
-                        intent,
-                        "اشتراک PDF"
-                )
-        );
-    }
+//    private void sharePdf(File file) {
+//
+//        Uri uri =
+//                FileProvider.getUriForFile(
+//                        this,
+//                        getPackageName() + ".provider",
+//                        file
+//                );
+//        Intent intent =
+//                new Intent(Intent.ACTION_SEND);
+//
+//        intent.setType("application/pdf");
+//
+//        intent.putExtra(
+//                Intent.EXTRA_STREAM,
+//                uri
+//        );
+//
+//        intent.addFlags(
+//                Intent.FLAG_GRANT_READ_URI_PERMISSION
+//        );
+//
+//        startActivity(
+//                Intent.createChooser(
+//                        intent,
+//                        "اشتراک PDF"
+//                )
+//        );
+//    }
     }
--- app/src/main/java/com/example/abgineh/OptimizActivity.java
+++ app/src/main/java/com/example/abgineh/OptimizActivity.java
@@ -1,16 +1,27 @@
 package com.example.abgineh;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.abgineh.myapp.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
+import java.util.Random;
 
 
 public class OptimizActivity extends AppCompatActivity {
@@ -18,6 +29,7 @@ public class OptimizActivity extends AppCompatActivity {
     Button btnNext;
     Button btnPrev;
     SheetView sheetView;
+    Button btnPdf;
     List<List<Placement>> sheets;
 
     GlassOptimizer optimizer;
@@ -30,12 +42,12 @@ public class OptimizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
           setContentView(R.layout.activity_optimiz);
-
+        sheetView = findViewById(R.id.sheetView);
         btnNext = findViewById(R.id.btnNext);
         btnPrev = findViewById(R.id.btnPrev);
-        sheetView = findViewById(R.id.sheetView);
-        pageNum=findViewById(R.id.pageNum);
 
+        pageNum=findViewById(R.id.pageNum);
+        btnPdf=findViewById(R.id.btnPdf);
         pieces = (List<GlassPiece>)
                 getIntent().getSerializableExtra("pieces");
 
@@ -87,6 +99,277 @@ private  void runOptimaiz(){
     Toast.makeText(OptimizActivity.this, "بهینه انجام شد", Toast.LENGTH_SHORT).show();
 
 
-}
+
+  btnNext.setOnClickListener(
+    view -> {
+
+        if (sheets == null) return;
+
+        if (currentSheet
+                < sheets.size() - 1) {
+
+            currentSheet++;
+
+            showSheet(currentSheet);
+        }
+    });
+
+        btnPrev.setOnClickListener(
+    view -> {
+
+        if (sheets == null) return;
+
+        if (currentSheet > 0) {
+
+            currentSheet--;
+
+            showSheet(currentSheet);
+        }
+    });
+
+    btnPdf.setOnClickListener(
+            view -> {
+
+                if (sheets != null) {
+                    exportPdf();
+                }
+            });
+
 
     }
+    private void exportPdf() {
+
+        PdfDocument document =
+                new PdfDocument();
+
+        int pageNum = 1;
+
+        Random random =
+                new Random();
+
+        for (List<Placement> sheet : sheets) {
+
+            PdfDocument.PageInfo pageInfo =
+                    new PdfDocument.PageInfo.Builder(
+                            1200,
+                            1600,
+                            pageNum
+                    ).create();
+
+            PdfDocument.Page page =
+
+                    document.startPage(pageInfo);
+
+            Canvas canvas =
+                    page.getCanvas();
+
+            Paint fillPaint =
+                    new Paint();
+
+            Paint borderPaint =
+                    new Paint();
+
+            Paint textPaint =
+                    new Paint();
+
+            borderPaint.setStyle(
+                    Paint.Style.STROKE
+            );
+
+            borderPaint.setStrokeWidth(4);
+
+            textPaint.setColor(Color.BLACK);
+
+
+            textPaint.setTextSize(28);
+
+            int margin = 80;
+
+            float scaleX =
+                    (1200f - margin * 2)
+                            / optimizer.sheetWidth;
+
+            float scaleY =
+                    (1500f - margin * 2)
+                            / optimizer.sheetHeight;
+
+            float scale =
+                    Math.min(scaleX, scaleY);
+
+            // کادر شیت
+
+            canvas.drawRect(
+                    margin,
+                    margin,
+
+
+                    margin +
+                            optimizer.sheetWidth * scale,
+
+                    margin +
+                            optimizer.sheetHeight * scale,
+
+                    borderPaint
+            );
+
+            for (Placement p : sheet) {
+
+                fillPaint.setColor(
+                        Color.rgb(
+                                random.nextInt(200),
+                                random.nextInt(200),
+                                random.nextInt(200)
+                        )
+                );
+
+                float left =
+
+                        margin + p.x * scale;
+
+                float top =
+                        margin + p.y * scale;
+
+                float right =
+                        margin +
+                                (p.x + p.width) * scale;
+
+                float bottom =
+                        margin +
+                                (p.y + p.height) * scale;
+
+                // رنگ
+
+                canvas.drawRect(
+                        left,
+                        top,
+                        right,
+                        bottom,
+                        fillPaint
+
+                );
+
+                // خط دور
+
+                if (p.rotated) {
+
+                    borderPaint.setColor(
+                            Color.RED
+                    );
+
+                } else {
+
+                    borderPaint.setColor(
+                            Color.BLACK
+                    );
+                }
+
+                canvas.drawRect(
+                        left,
+                        top,
+                        right,
+
+                        bottom,
+                        borderPaint
+                );
+
+                // متن ابعاد
+
+                canvas.drawText(
+
+                        p.width +
+                                "×" +
+                                p.height,
+
+                        left + 10,
+
+                        top + 35,
+
+                        textPaint
+                );
+            }
+
+            // عنوان صفحه
+
+
+            canvas.drawText(
+
+                    "Sheet " + pageNum,
+
+                    80,
+
+                    40,
+
+                    textPaint
+            );
+
+            document.finishPage(page);
+
+            pageNum++;
+        }
+
+        try {
+
+            File file =
+                    new File(
+
+
+                            getExternalFilesDir(
+                                    Environment
+                                            .DIRECTORY_DOCUMENTS
+                            ),
+
+                            "glass_output.pdf"
+                    );
+
+            document.writeTo(
+                    new FileOutputStream(file)
+            );
+
+            document.close();
+
+            sharePdf(file);
+
+//            txtInfo.setText(
+//                    "PDF ساخته شد"
+//            );
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+//            txtInfo.setText(
+//                    e.getMessage()
+//            );
+        }
+    }
+    private void sharePdf(File file) {
+
+        Uri uri =
+                FileProvider.getUriForFile(
+                        this,
+                        getPackageName() + ".provider",
+                        file
+                );
+        Intent intent =
+                new Intent(Intent.ACTION_SEND);
+
+        intent.setType("application/pdf");
+
+        intent.putExtra(
+                Intent.EXTRA_STREAM,
+                uri
+        );
+
+        intent.addFlags(
+                Intent.FLAG_GRANT_READ_URI_PERMISSION
+        );
+
+        startActivity(
+                Intent.createChooser(
+                        intent,
+                        "اشتراک PDF"
+                )
+        );
+    }
+}
